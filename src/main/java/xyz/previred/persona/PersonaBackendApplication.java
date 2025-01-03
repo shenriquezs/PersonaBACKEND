5224ae061893e414c9bfe1288ab6d0d2663eb463
shenriquezs <shenriquezs@tgr.cl>
2025-01-03 09:11:14 -0300

    resubida

--- src/main/java/xyz/previred/persona/PersonaBackendApplication.java
+++ src/main/java/xyz/previred/persona/PersonaBackendApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
 @SpringBootApplication
 public class PersonaBackendApplication {
 
